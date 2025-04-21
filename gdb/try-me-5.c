7126e1d7ab7828522ca477ce4816fb7731610316
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-21 21:10:03 +0530

    Create try-me-5.c

---
+++ gdb/try-me-5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main()
+{
+  FILE *pfile;
+  int ch;
+
+  puts("Open the FILE.TXT file ...");
+
+  pfile = fopen("FILE.TXT", "r");
+
+  /* you should check if the file pointer is valid, but we skipped that */
+
+  puts("Now display the contents of FILE.TXT ...");
+
+  while ((ch = fgetc(pfile)) != EOF) {
+    printf("<%c>", ch);
+  }
+
+  fclose(pfile);
+
+  /* done */
+
+  puts("Ok");
+  return 0;
+}
